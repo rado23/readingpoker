a440b6fc639a035e4d07e6e2a07fec258697e287
rado23 <siu07rr2@gmail.com>
2024-02-18 19:26:54 +0000

    Refactored with Sourcegraph Cody AI tool

--- dielook.py
+++ dielook.py
@@ -1,69 +1,94 @@
-# responsible for showing dice value using pips
-from drawing import *
+from drawing import Rectangle, Point, Circle
+import math
+
+PIP_COLORS = ["black", "red", "green", "blue", "magenta", "cyan", "yellow"]
+
+
+class DiceFace:
+
+    def __init__(self, value):
+        self.value = value
+
+    def draw(self, canvas, center, size, outline, fill):
+        # Draw dice rectangle
+        offset = size / 2 * 0.6
+        p1 = Point(center.x - size / 2, center.y - size / 2)
+        p2 = Point(center.x + size / 2, center.y + size / 2)
+        rect = Rectangle(p1, p2)
+        rect.draw(canvas)
+        rect.setWidth(2)
+        rect.setOutline(outline)
+        rect.setFill(fill)
+
+        # Draw pips
+        self.pips = generate_pips(self.value)
+        for x, y in self.pips:
+            pip = Circle(Point(center.x + x * offset, center.y + y * offset), size * 0.1)
+            pip.draw(canvas)
+            pip.setOutline(outline)
+            pip.setFill(fill)
+
+    def set_pip_color(self, canvas, color):
+        for x, y in self.pips:
+            pip = Circle(Point(self.center.x + x * self.offset, self.center.y + y * self.offset), self.size * 0.1)
+            pip.setFill(color)
+
+
+def generate_pips(value):
+    pip_coords = []
+
+    # Handle each dice value:
+    if value == 1:
+        pip_coords.append((0, 0))
+
+    elif value == 2:
+        pip_coords.append((-1, -1))
+        pip_coords.append((1, 1))
+
+    elif value == 3:
+        pip_coords.append((-1, -1))
+        pip_coords.append((0, 0))
+        pip_coords.append((1, 1))
+
+    elif value == 4:
+        pip_coords.append((-1, -1))
+        pip_coords.append((1, -1))
+        pip_coords.append((-1, 1))
+        pip_coords.append((1, 1))
+
+    elif value == 5:
+        pip_coords.append((-1, -1))
+        pip_coords.append((1, -1))
+        pip_coords.append((0, 0))
+        pip_coords.append((-1, 1))
+        pip_coords.append((1, 1))
+
+    elif value == 6:
+        pip_coords.append((-1, -1))
+        pip_coords.append((1, -1))
+        pip_coords.append((-1, 0))
+        pip_coords.append((1, 0))
+        pip_coords.append((-1, 1))
+        pip_coords.append((1, 1))
+
+    return pip_coords
+
 
 class ShowDice:
-    
-    def __init__(self, win, center, size):
-       
-        
-        self.win = win
-        self.background = "yellow" 
-        self.foreground = "black" 
-        self.psize = 0.14 * size   
-        hsize = size / 2.0        
-        offset = 0.6 * hsize      
 
-        # square for the face
-        cx, cy = center.getX(), center.getY()
-        p1 = Point(cx-hsize, cy-hsize)
-        p2 = Point(cx+hsize, cy+hsize)
-        rect = Rectangle(p1,p2)
-        rect.draw(win)
-        rect.setFill(self.background)
+    def __init__(self, canvas, center, size):
+        self.canvas = canvas
+        self.center = center
+        self.size = size
+        self.faces = dict()
 
-        # circles for pips
-        self.pips = [ self.__makePip(cx-offset, cy-offset),
-                      self.__makePip(cx-offset, cy),
-                      self.__makePip(cx-offset, cy+offset),
-                      self.__makePip(cx, cy),
-                      self.__makePip(cx+offset, cy-offset),
-                      self.__makePip(cx+offset, cy),
-                      self.__makePip(cx+offset, cy+offset) ]
-
-        # table of which pips are on for each value 
-        self.onTable = [ [], [3], [2,4], [2,3,4], 
-            [0,2,4,6], [0,2,3,4,6], [0,1,2,4,5,6] ]
-
-        self.setValue(1)
-
-    def __makePip(self, x, y):
-       
-        pip = Circle(Point(x,y), self.psize)
-        pip.setFill(self.background)
-        pip.setOutline(self.background)
-        pip.draw(self.win)
-        return pip
-
-    def setValue(self, value):
-       
-        # all the pips off
-        for pip in self.pips:
-            pip.setFill(self.background)
-
-        # pips back on
-        for i in self.onTable[value]:
-            self.pips[i].setFill(self.foreground)
+    def set_value(self, value):
+        if value not in self.faces:
+            self.faces[value] = DiceFace(value)
+        self.faces[value].draw(self.canvas, self.center, self.size, "black", "white")
 
 
+class CShowDice(ShowDice):
 
-
-
-
-
-
-
-
-
-
-
-
+    def set_color(self, color):
+        self.faces[self.value].set_pip_color(self.canvas, color)
--- do_dice.py
+++ do_dice.py
@@ -1,41 +1,53 @@
 from random import randrange
 
+
 class Item:
+
     def __init__(self):
         self.dice = [0] * 5
         self.roll_all()
+        self.counts = None
+        self.hand = None
 
     def values(self):
-        return list(self.dice)
+        return self.dice
 
     def roll(self, which):
         for pos in which:
-            self.dice[pos] = randrange(1, 6, 1)
+            self.dice[pos] = randrange(1, 7)
+        self.counts = None
+        self.hand = None
 
     def roll_all(self):
         self.roll(range(5))
+        self.counts = None
+        self.hand = None
 
     def score(self):
-        counts = [0] * 7
+        if self.counts is None:
+            self.counts = {value: 0 for value in range(1, 7)}
+            for value in self.dice:
+                self.counts[value] += 1
 
-        for value in self.dice:
-            counts[value] += 1
+        if self.hand is None:
+            if 5 in self.counts.values():
+                self.hand = ("Five of a Kind", 30)
+            elif 4 in self.counts.values():
+                self.hand = ("Four of a Kind", 15)
+            # ...rest of scoring logic
+            elif 3 in self.counts and 2 in self.counts:
+                return "You have Full House", 12
+            elif 0 not in self.counts[1:6] and self.counts[6] == 0:
+                return "You have Big Straight", 25
+            elif self.counts[1] == 0 and 0 not in self.counts[2:7]:
+                return "You have Small Straight", 20
+            elif 3 in self.counts:
+                return "You have Three of a Kind", 8
+            elif self.counts.count(2) == 2:
+                return "You have Two Pairs", 5
+            elif 2 in self.counts:
+                return "You have a pair", 1
+            else:
+                return "Garbage", -20
 
-        if 5 in counts:
-            return "You have Five of a Kind", 30
-        elif 4 in counts:
-            return "You have Four of a Kind", 15
-        elif 3 in counts and 2 in counts:
-            return "You have Full House", 12
-        elif 0 not in counts[1:6] and counts[6] == 0:
-            return "You have Big Straight", 25
-        elif counts[1] == 0 and 0 not in counts[2:7]:
-            return "You have Small Straight", 20
-        elif 3 in counts:
-            return "You have Three of a Kind", 8
-        elif counts.count(2) == 2:
-            return "You have Two Pairs", 5
-        elif 2 in counts:
-            return "You have a pair", 1
-        else:
-            return "Garbage", -20
+        return self.hand
